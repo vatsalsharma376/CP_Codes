8af56aacf9a58e0a0e82e4bdc2b6a4a5c115b73a
Vatsal Sharma <vatsalsharma376@gmail.com>
2018-12-12 19:44:10 +0530

    Update SPOJ_ADATRIP.cpp

--- SPOJ_ADATRIP.cpp
+++ SPOJ_ADATRIP.cpp
@@ -1,26 +1,4 @@
-/*input
-10 10 10
-1 1 1
-1 2 1
-1 2 3 
-3 1 1
-5 4 10
-8 5 10
-5 6 5
-6 7 3
-6 9 3
-9 7 4
-0
-1
-2
-3
-4
-5
-6
-7
-8
-9
-*/
+
 
 #include <bits/stdc++.h>
 #define getcx getchar_unlocked
@@ -54,12 +32,12 @@ void dijk(int src){
 		ii cur = pq.top(); pq.pop();
 		int curn = cur.second,curd=cur.first;
 		if(vis[curn]) continue;
-		vis[curn]=1;
+		vis[curn]=1;	
 		for(auto v:grp[curn]){
 			
-				if(dist[v.second]>curd+v.first){
-					dist[v.second]=curd+v.first;
-					pq.push({dist[v.second],v.second});
+				if(dist[v.first]>dist[curn]+v.second){
+					dist[v.first]=dist[curn]+v.second;
+					pq.push({dist[v.first],v.first});
 				}
 			
 		}
@@ -78,23 +56,21 @@ signed main(){
 		// now find max in dist[] array = mx
 		// find number of entries dist[] array that are mx
 		// it is the answer
-		// we can use dial algo because of small weight or traditional dijkstra implementation(loose TL)
+		// we can use dial algo because of small weight or maybe very optimised implementation
 		int node; cin>>node;
 		dist.resize(n,INT_MAX);
 		vis.resize(n,0);
 		dijk(node);
 
 		int mx=-1;
-		
-		int ans=0;
 		fors(j,0,n){
-			if(dist[i]==INT_MAX) continue;
-			if(dist[i]==mx) ++ans;
-			else if(dist[i]>mx){
-				mx=dist[i];
-				ans=1;
-			}
+			if(dist[j]==INT_MAX) continue;
+			mx=max(mx,dist[j]);
 		}
+		int ans=0;
+		fors(j,0,n)
+			if(dist[j]==mx) ++ans;
+		
 		cout<<mx<<" "<<ans<<"\n";
 		vis.clear();
 		dist.clear();
